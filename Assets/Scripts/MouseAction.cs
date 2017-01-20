78d3e19eea68b5920bc3da0509f3d006f91cae39
Francisco García <pakiyo_pgd@hotmail.com>
2017-01-20 21:59:16 +0100

    Añadido raycast

---
+++ Assets/Scripts/MouseAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAction : MonoBehaviour {
+
+    [SerializeField] private float MaxDistance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, MaxDistance))
+            print("Found an object - distance: " + hit.distance);
+
+        Debug.DrawRay(transform.position, transform.forward , Color.green);
+    }
+}
