b5d9814b0c99f1c44a01a861bbc93b57563b6159
Alex <afcarrion85@gmail.com>
2017-01-21 12:55:46 +0100

    light attached

--- Assets/Scripts/LightScripts/LigthMovement.cs
+++ Assets/Scripts/LightScripts/LigthMovement.cs
@@ -5,32 +5,40 @@ using UnityEngine;
 public class LigthMovement : MonoBehaviour {
 
 	private bool overlap = false;
-	public float speed = 5;
+	public float speed = 8;
 	private Vector3 direction;
-	public float delay = 4;
-	private float timeDelay = 0;
-	private bool isStopped = true;
 
 	public Transform center;
+	public Transform bind;
+	private bool isAttached = false;
 
 	void Start () {}
 
 	void Update () {
-		if (overlap) {
-			Debug.Log (direction);
-			transform.Translate(direction * Time.deltaTime, Space.World);
+		if (!isAttached) {
+			if (overlap) {
+				transform.Translate (direction * Time.deltaTime, Space.World);
+			} else {
+				transform.position = Vector3.MoveTowards (transform.position, center.transform.position, speed * Time.deltaTime);
+			}
 		} else {
-			transform.position = Vector3.MoveTowards (transform.position, center.transform.position, speed * Time.deltaTime);
+			transform.position = Vector3.MoveTowards (transform.position, bind.transform.position, speed * Time.deltaTime);
 		}
 	}
 
 	void OnTriggerStay(Collider other) {
-		float x = transform.position.x - other.transform.position.x;
-		float z = transform.position.z - other.transform.position.z;
-		direction.Set (x,0,z);
+		if (!isAttached) {
+			float x = transform.position.x - other.transform.position.x;
+			float z = transform.position.z - other.transform.position.z;
+			direction.Set (x, 0, z);
+		}
 	}
 		
 	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Finish") {
+			Debug.Log (other.tag);
+			isAttached = true;
+		}
 		overlap = true;
 	}
 
