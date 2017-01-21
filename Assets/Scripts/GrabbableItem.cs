7786ead62c11b7c81ca03ec4777b0075bebe56d2
Francisco García <pakiyo_pgd@hotmail.com>
2017-01-21 09:37:03 +0100

    Script dragAndDrop funcionando

---
+++ Assets/Scripts/GrabbableItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableItem : MonoBehaviour {
+
+    GameObject player = null;
+    Rigidbody rb;
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    public void Grab(GameObject player)
+    {
+        this.player = player;
+        this.transform.parent = player.transform;
+        rb.isKinematic = true;
+        
+    }
+
+    public void Drop()
+    {
+        this.transform.parent = null;
+        this.player = null;
+        rb.isKinematic = false;
+    }
+}
--- Assets/Scripts/MouseAction.cs
+++ Assets/Scripts/MouseAction.cs
@@ -1,25 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseAction : MonoBehaviour {
 
-    [SerializeField] private float MaxDistance;
-	// Use this for initialization
-	void Start () {
-		
-	}
 
-    // Update is called once per frame
+    [SerializeField]
+    private float interactionDistance = 3f;
+
+    [SerializeField]
+    private Image imagen;
+
+    GrabbableItem gi;
 
     void FixedUpdate()
     {
         RaycastHit hit;
 
-        if(Input.GetButtonUp("Accion"))
-            if (Physics.Raycast(transform.position, transform.forward, out hit, MaxDistance))
-                print("Found an object - distance: " + hit.distance);
+        Vector3 forward = transform.forward;
 
-        Debug.DrawRay(transform.position, transform.forward , Color.green);
+        Debug.DrawRay(transform.position, forward * interactionDistance);
+
+        if (Input.GetButtonDown("Accion") && gi != null)
+        {
+            gi.Drop();
+            gi = null;
+        }
+        else
+        {
+            if (gi == null)
+            {
+                if (Physics.Raycast(transform.position, forward, out hit, interactionDistance))
+                {
+                    imagen.color = Color.green;
+                    print("Found an object - distance: " + hit.collider.gameObject.ToString());
+
+                    if (Input.GetButtonDown("Accion"))
+                    {
+                        gi = hit.collider.gameObject.GetComponent<GrabbableItem>();
+                        if (gi != null)
+                        {
+                            gi.Grab(gameObject);
+                        }
+                    }
+
+                }
+                else
+                {
+                    imagen.color = Color.red;
+                }
+            }
+            else
+            {
+                imagen.color = Color.clear;
+            }
+        }
+
+
     }
+
+
+
 }
