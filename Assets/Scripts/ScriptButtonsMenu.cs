845996349eec2b22df0c6f68af9ce85adb46ebaa
Kam\K <kamryner@gmail.com>
2017-01-21 13:32:46 +0100

    Main Menu

---
+++ Assets/Scripts/ScriptButtonsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScriptButtonsMenu : MonoBehaviour {
+
+	public void salir()
+    {
+        Application.Quit();
+    }
+
+    public void jugar()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
