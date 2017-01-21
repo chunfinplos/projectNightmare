26b6fd312f2b94cfffc33c4e8521d2fb05b732fa
Alex <afcarrion85@gmail.com>
2017-01-21 19:12:16 +0100

    fade

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour {
+
+	//private Material m_Material = null;
+	private bool m_Fading = false;
+	public float aFadeOutTime = 5.0f;
+	public float aFadeInTime = 5.0f;
+	public Color aColor = Color.white;
+
+	void Start () {}
+
+	void Update () {}
+
+	private void DrawQuad (Color aColor, float aAlpha) {
+		aColor.a = aAlpha;
+		//m_Material.SetPass (0);
+		GL.PushMatrix ();
+		GL.LoadOrtho ();
+		GL.Begin (GL.QUADS);
+		GL.Color (aColor);   // moved here, needs to be inside begin/end
+		GL.Vertex3 (0, 0, -1);
+		GL.Vertex3 (0, 1, -1);
+		GL.Vertex3 (1, 1, -1);
+		GL.Vertex3 (1, 0, -1);
+		GL.End ();
+		GL.PopMatrix ();
+	}
+
+	public void StartFade () {
+		m_Fading = true;
+		StartCoroutine (DoFade (aFadeOutTime, aFadeInTime, aColor));
+	}
+
+	public bool IsFading() {
+		return m_Fading;
+	}
+
+	private IEnumerator DoFade (float aFadeOutTime, float aFadeInTime, Color aColor) {
+		float t = 0.0f;
+		while (t < 1.0f) {
+			yield return new WaitForEndOfFrame ();
+			t = Mathf.Clamp01 (t + Time.deltaTime / aFadeOutTime);
+			DrawQuad (aColor, t);
+		}
+		while (t > 0.0f) {
+			yield return new WaitForEndOfFrame ();
+			t = Mathf.Clamp01 (t - Time.deltaTime / aFadeInTime);
+			DrawQuad (aColor, t);
+		}
+		m_Fading = false;
+	}
+}
--- Assets/Scripts/LightScripts/ManagerLight.cs
+++ Assets/Scripts/LightScripts/ManagerLight.cs
@@ -7,10 +7,17 @@ public class ManagerLight : MonoBehaviour {
 	public GameObject[] lights = new GameObject[5];
 	private int index = 1;
 	public float timeDelay = 5;
+	private bool finish = false;
 	
 	void Start () {}
 
-	void Update () {}
+	void Update () {
+		if (finish) {
+			finish = false;
+			Fade fade = GetComponent<Fade> ();
+			fade.StartFade ();
+		}
+	}
 
 	public void attachLight(string name) {
 		Debug.Log (name);
@@ -19,12 +26,14 @@ public class ManagerLight : MonoBehaviour {
 
 	IEnumerator delay() {
 		yield return new WaitForSeconds(timeDelay);
-		if(index < lights.Length) {
+		if (index < lights.Length) {
 			lights [index].SetActive (true);
 
-			AudioSource audio = GetComponent<AudioSource>();
+			AudioSource audio = GetComponent<AudioSource> ();
 			audio.Play ();
+			index++;
+		} else {
+			finish = true;
 		}
-		index++;
 	}
 }
